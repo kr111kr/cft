6500a7d9307f8d53147b97c59516e835c5a29ebb
Kristina Popova <kr111kr@yandex.ru>
2019-02-27 15:03:13 +0700

    1
    one

---
+++ tableMultiplication/src/ru/nsu/popova/objects/Table.java
@@ -0,0 +1,22 @@
+package ru.nsu.popova.objects;
+
+public class Table {
+    private int size;
+    private int lenMaxValue;
+
+    public int getLenMaxValue() {
+        return lenMaxValue;
+    }
+
+    public void setLenMaxValue(int lenMaxValue) {
+        this.lenMaxValue = lenMaxValue;
+    }
+
+    public int getTableSize() {
+        return size;
+    }
+
+    public void setTableSize(int tableSize) {
+        this.size = tableSize;
+    }
+}
